163e37de67a679c5f86c66dd8237900014d3e95a
mariem-art <71516261+mariem-art@users.noreply.github.com>
2024-04-30 16:15:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+//récupérer les données de fihier recettes.js
+async function displayRecipes(recipes) {
+  const recettSection = document.querySelector(".choix-recette");
+  const recipeCountElement = document.getElementById('nb');
+  recipeCountElement.textContent = recipes.length;
+  
+  for (const recipe of recipes) {
+   recipes.forEach((recipes) => {
+      const recettModel = recettTemplate(recipes);
+      const userCardDOM = recettModel.getUserCardDOM();
+      recettSection.appendChild(userCardDOM);
+    });
+  }
+}
+async function init() {
+  // Récupère les datas des photographes
+  displayRecipes(recipes);
+  
+}
+init();   
+//search
+async function getRecipe() { 
+ let newDataReciepes = recipes;
+ return newDataReciepes;
+}
+
+// Appeler la fonction pour récupérer les recettes et ensuite appeler searchBar
+
+async function initializeSearch() {
+// Récupérer les recettes
+const allRecipes = await getRecipe();
+// Appeler searchBar avec les recettes récupérées
+searchBar(allRecipes);
+
+}
+
+// Appeler la fonction d'initialisation
+//initializeSearch();
+
+
---
+++ value.js
@@ -0,0 +1,45 @@
+//const
+const recipeContainer = document.getElementById("recipeContainer");
+const inputSuggestion = document.querySelectorAll("little-search");
+const clearInput = document.getElementById("clearInput");
+const clearIcon = document.querySelector("btn.btn-outline-success");
+const messageError = document.querySelector(".message-error");
+const recipeCountElement = document.getElementById("recipeCount");
+const tagsContainer = document.getElementById("nb-recette");
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Dropdown Ingrédients
+  const iconIn = document.querySelector('.fa-solid.fa-chevron-up');
+  const searchInputIn = document.getElementById('liste-choix-Ing');
+  iconIn.addEventListener('click', function() {
+      iconIn.classList.toggle('rotate');
+      searchInputIn.classList.toggle('show');
+      if (searchInputIn.classList.contains('show')) {
+          searchInputIn.focus(); 
+      }
+  });
+   // Dropdown Appareils
+   const iconApp = document.querySelector('span.fa-solid.fa-chevron-up.App');
+   const searchInputApp = document.getElementById('liste-choix-App');
+   iconApp.addEventListener('click', function() {
+       iconApp.classList.toggle('rotate');
+       searchInputApp.classList.toggle('show');
+       if (searchInputApp.classList.contains('show')) {
+           searchInputApp.focus(); 
+       }
+   });
+
+    // Dropdown Ustensiles
+    const iconUst = document.querySelector('.fa-solid.fa-chevron-up.Ust');
+    const searchInputUst = document.getElementById('liste-choix-Ust');
+    iconUst.addEventListener('click', function() {
+        iconUst.classList.toggle('rotate');
+        searchInputUst.classList.toggle('show');
+        if (searchInputUst.classList.contains('show')) {
+            searchInputUst.focus(); 
+        }
+    });
+});
+
+
+
